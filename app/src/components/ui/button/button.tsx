9ac7f6f923ed7d82f3800335b8576b955bf5c784
sh-okada <sh-okada@enecom.co.jp>
2024-11-21 17:11:47 +0900

    buttonコンポーネント作成

--- app/src/components/ui/button/button.tsx
+++ app/src/components/ui/button/button.tsx
@@ -1,10 +1,53 @@
 import { Button as HeadlessuiButton } from "@headlessui/react";
 import React from "react";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonVariant = "fill" | "outline";
+type ButtonSize = "lg" | "md" | "sm" | "xs";
 
-const Button = ({ ...props }: ButtonProps) => {
-  return <HeadlessuiButton {...props} />;
+const buttonBaseStyle = `
+p-2
+rounded
+`;
+
+const buttonVariantStyle: { [key in ButtonVariant]: string } = {
+  fill: `
+  bg-primary-500
+  text-primary-50
+  hover:bg-primary-600
+  hover:underline
+  active:bg-primary-700
+  active:underline
+  data-disabled:bg-primary-100
+  data-disabled:text-primary-400
+  `,
+  outline: `
+  border
+  border-primary-600
+  text-primary-600
+  hover:border-primary-700
+  hover:text-primary-700
+  hover:underline  
+  active:border-primary-800
+  active:text-primary-800
+  active:underline
+  data-disabled:border-primary-200
+  data-disabled:text-primary-200
+  `,
 };
 
-export { Button, type ButtonProps };
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: ButtonVariant;
+};
+
+const Button = ({ className, variant = "fill", ...props }: ButtonProps) => {
+  return (
+    <HeadlessuiButton
+      className={`${buttonBaseStyle} ${buttonVariantStyle[variant]} ${
+        className ?? ""
+      }`}
+      {...props}
+    />
+  );
+};
+
+export { Button, type ButtonProps, type ButtonVariant, type ButtonSize };
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    data: {
+      disabled: "disabled",
+    },
     extend: {},
+    // https://uicolors.app/create #f7f7ed
+    colors: {
+      primary: {
+        50: "#f7f7ed",
+        100: "#ededd8",
+        200: "#dadaad",
+        300: "#c6c383",
+        400: "#b9b366",
+        500: "#ad9e53",
+        600: "#988247",
+        700: "#7f673e",
+        800: "#695437",
+        900: "#58462f",
+        950: "#312517",
+      },
+    },
   },
   plugins: [],
 };
