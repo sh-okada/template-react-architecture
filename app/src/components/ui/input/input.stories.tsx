445c319967a961cca41d39229d7d48bab02e522d
sh-okada <sh-okada@enecom.co.jp>
2024-11-26 08:55:21 +0900

    FieldとLabelコンポーネントを使った例を追加

--- app/src/components/ui/input/input.stories.tsx
+++ app/src/components/ui/input/input.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Input } from "./input";
+import { Label } from "../label";
+import { Field } from "../field";
 
 const meta: Meta<typeof Input> = {
   component: Input,
@@ -9,3 +11,18 @@ export default meta;
 type Story = StoryObj<typeof Input>;
 
 export const Default: Story = {};
+
+export const FieldInput: Story = {
+  render: (args) => {
+    return (
+      <Field>
+        <Label>名前</Label>
+        <Input {...args} />
+      </Field>
+    );
+  },
+  args: {
+    className: "block",
+    placeholder: "岡田修太",
+  },
+};
