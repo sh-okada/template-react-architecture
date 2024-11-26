74fb315c92d9e1314db0640de321e8478abb5137
sh-okada <sh-okada@ymail.ne.jp>
2024-11-26 22:10:04 +0900

    レイアウト修正

--- app/src/components/ui/markdown-editor/markdown-editor.tsx
+++ app/src/components/ui/markdown-editor/markdown-editor.tsx
@@ -3,16 +3,18 @@ import {
   useMarkdownEditor,
 } from "../../../hooks/use-markdown-editor";
 
-type MarkdownEditorProps = {
+type MarkdownEditorProps = Omit<React.ComponentProps<"div">, "ref"> & {
   doc: string;
   onChangeDoc: OnChangeDoc;
   placeholder?: string;
 };
 
 const MarkdownEditor = ({
+  className,
   doc,
   onChangeDoc,
   placeholder,
+  ...props
 }: MarkdownEditorProps) => {
   const { editor } = useMarkdownEditor({
     doc: doc,
@@ -20,7 +22,13 @@ const MarkdownEditor = ({
     placeholderText: placeholder,
   });
 
-  return <div ref={editor} />;
+  return (
+    <div
+      className={`p-2 border-primary-600 rounded-lg border ${className ?? ""}`}
+      ref={editor}
+      {...props}
+    />
+  );
 };
 
 export { MarkdownEditor, type MarkdownEditorProps };
