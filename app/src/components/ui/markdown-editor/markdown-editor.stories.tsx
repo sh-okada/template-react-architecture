225f0d136b9132a1bc6c2f608642ef4c517a3466
sh-okada <sh-okada@ymail.ne.jp>
2024-11-26 22:25:40 +0900

    スタイルを利用したストーリー追加

--- app/src/components/ui/markdown-editor/markdown-editor.stories.tsx
+++ app/src/components/ui/markdown-editor/markdown-editor.stories.tsx
@@ -3,15 +3,27 @@ import { MarkdownEditor } from "./markdown-editor";
 
 const meta: Meta<typeof MarkdownEditor> = {
   component: MarkdownEditor,
+  args: {
+    doc: "",
+    onChangeDoc: (doc) => console.log(doc),
+    placeholder: "content here.",
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof MarkdownEditor>;
 
-export const Default: Story = {
+export const Default: Story = {};
+
+export const WithStyle: Story = {
   args: {
-    doc: "",
-    onChangeDoc: (doc) => console.log(doc),
-    placeholder: "content here.",
+    editorStyle: {
+      "&": {
+        minHeight: "500px",
+      },
+      "&.cm-editor": {
+        outline: "none",
+      },
+    },
   },
 };
